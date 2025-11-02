9cd756be62acb2174030238f62fb322e7e684bc5
mithun4434 <96248479+mithun4434@users.noreply.github.com>
2025-11-02 21:49:09 +0530

    Delete types.ts

--- types.ts
+++
@@ -1,7 +0,0 @@
-// Fix: Added the missing 'InputMode' type definition.
-export type InputMode = 'text' | 'upload' | 'camera';
-
-export interface Solution {
-  answer: string;
-  steps: string[];
-}
